e6e3b93bd65a1868b693609292ad23624022ef69
pbrudny <pbrudny@gmail.com>
2017-10-08 16:00:05 +0200

    Add plugins from 4.5

--- webpack_4_3/webpack.config.js
+++ webpack_4_3/webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
 module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    ]
 };
