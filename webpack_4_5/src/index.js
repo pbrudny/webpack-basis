6454432ffe75fb1d6599e1d27237a3b2f290995d
pbrudny <pbrudny@gmail.com>
2017-10-11 21:41:35 +0200

    Add Hot Module Replacement for React

--- webpack_4_5/src/index.js
+++ webpack_4_5/src/index.js
@@ -1,8 +1,22 @@
+import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
 ReactDOM.render(
-  <App />,
+    <App />,
     document.getElementById('app')
 );
+
+if (module.hot) {
+    module.hot.accept('./containers/App', () => {
+        const NextApp = require('./containers/App').default;
+        ReactDOM.render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            document.getElementById('app')
+        );
+
+    });
+}
--- webpack_4_5/webpack.config.js
+++ webpack_4_5/webpack.config.js
@@ -24,7 +24,10 @@ if (env === 'development') {
 }
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        'react-hot-loader/patch',
+        './src/index.js'
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
